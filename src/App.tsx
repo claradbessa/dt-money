45a77b3a4afc08f95c0a70cc732edd493c90c27b
Clara Bessa <clarabessa@outlook.com>
2022-06-10 14:22:52 -0300

    configurando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 
 import { GlobalStyle } from './sytles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [ isNewTransictionModalOpen, setIsNewTransictionModalOpen ] = useState(false);
+
+  function handleOpenNewTransictionModal() {
+      setIsNewTransictionModalOpen(true);
+  }
+
+  function handleCloseNewTransictionModal() {
+      setIsNewTransictionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransictionModal} />
       <Dashboard />
+
+      <Modal 
+        isOpen={isNewTransictionModalOpen} 
+        onRequestClose={handleCloseNewTransictionModal}
+        >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
