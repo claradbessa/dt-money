21ce28bb478c22178e468ebb431d27c456345bdf
Clara Bessa <clarabessa@outlook.com>
2022-06-19 19:48:34 -0300

    botões entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -25,6 +27,19 @@ export function NewTransactionModal({ isOpen, onRequestClose} : NewTransactionMo
 
             <input placeholder="Título" />
             <input type="number" placeholder="Valor" />
+
+            <TransactionTypeContainer>
+                <button type="button">
+                    <img src={incomeImg} alt=""/>
+                    <span>Entrada</span>
+                </button>
+
+                <button type="button">
+                    <img src={outcomeImg} alt=""/>
+                    <span>Saída</span>
+                </button>
+            </TransactionTypeContainer>
+
             <input placeholder="Categoria" />
 
             <button type="submit">Cadastrar</button>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {darken} from 'polished';
 
 export const Container = styled.form`
     h2{
@@ -35,14 +36,52 @@ export const Container = styled.form`
         background: var(--green);
         color: #fff;
         border-radius: 0.25rem;
-        border: 0;
+        border: 025rem;
         font-size: 1rem;
         margin-top: 1.5rem;
+        font-weight: 600;
 
         transition: filter 0.2s;
 
         &:hover{
             filter: brightness(0.9);
+        }
+    }
+`;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5; 
+
+    button{
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transition: border-color 0.2s;
+
+        &:hover{
+            border-color: ${darken(0.1, '#d7d7d7')};
+        }
+
+        img{
+            height: 20px;
+            width: 20px;
+        }
+
+        span{
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
         }
     }
 `;
